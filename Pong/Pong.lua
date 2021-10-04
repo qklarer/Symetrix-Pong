e67807ea8229bc424164e4cf07c2728bfb9c9e45
Quinn Klarer <84090326+qklarer@users.noreply.github.com>
2021-10-04 11:25:43 -0700

    Add files via upload

--- Pong/Pong.lua
+++ Pong/Pong.lua
@@ -18,27 +18,30 @@ function TimerClick()
             end
         end
 
-        if row == 36 then 
+        if row >= 37 then 
             Volly1 = false
             Volly2 = true
+            if Padle2 ~= i then
+                NamedControl.SetText("Info", "Player 1 wins")
+            end
         end
 
         if Volly2 == true then
-            -- print(Padle2)
-            -- print(Padle1)
             if Padle2 == i then
                 row = (row - 1)
-                print(row)
                 NamedControl.SetPosition("m" .. i .. "." .. row, 1)
                 NamedControl.SetPosition("m" .. i .. "." .. (row + 1), 0)
             else
                 NamedControl.SetPosition("m" .. i .. "." .. row, 0)
             end
         end
-        
+
         if row == 1 then 
             Volly1 = true
             Volly2 = false
+            if Padle1 ~= i then
+                NamedControl.SetText("Info", "Player 2 wins")
+            end
         end 
     end
 end
