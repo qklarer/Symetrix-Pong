a6d1f4266aae465efb45e3483803f38ec4081497
Quinn Klarer <84090326+qklarer@users.noreply.github.com>
2021-10-04 09:56:11 -0700

    Add files via upload
    Created volly and ball.

--- Pong/Pong.lua
+++ Pong/Pong.lua
@@ -1,18 +1,45 @@
 row = 1
+Volly1 = true
 function TimerClick()
-    Padle1 =  (math.floor(NamedControl.GetValue("p1")))
-    if row > 36 then
-        row = 0
+    Padle1 = (math.floor(NamedControl.GetValue("p1")))
+    Padle2 = (math.floor(NamedControl.GetValue("p2")))
+    if row >= 37 then
+        row = 1
     end
 
-    for i = 1,21 do 
-        if Padle1 == i then
-            row = (row + 1)
-            NamedControl.SetPosition("m" .. i .. "." .. row, 1)
-            NamedControl.SetPosition("m" .. i .. "." .. (row - 1), 0)
-        else
-            NamedControl.SetPosition("m" .. i .. "." .. row, 0)
+    for i = 1,21 do
+        if Volly1 == true then
+            if Padle1 == i then
+                row = (row + 1)
+                NamedControl.SetPosition("m" .. i .. "." .. row, 1)
+                NamedControl.SetPosition("m" .. i .. "." .. (row - 1), 0)
+            else
+                NamedControl.SetPosition("m" .. i .. "." .. row, 0)
+            end
         end
+
+        if row == 36 then 
+            Volly1 = false
+            Volly2 = true
+        end
+
+        if Volly2 == true then
+            -- print(Padle2)
+            -- print(Padle1)
+            if Padle2 == i then
+                row = (row - 1)
+                print(row)
+                NamedControl.SetPosition("m" .. i .. "." .. row, 1)
+                NamedControl.SetPosition("m" .. i .. "." .. (row + 1), 0)
+            else
+                NamedControl.SetPosition("m" .. i .. "." .. row, 0)
+            end
+        end
+        
+        if row == 1 then 
+            Volly1 = true
+            Volly2 = false
+        end 
     end
 end
 
